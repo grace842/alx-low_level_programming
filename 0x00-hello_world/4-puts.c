0c3a2eda38e94209c8b5de35037c9ea537206185
grace842 <gwanja30@gmail.com>
2023-04-20 07:17:46 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /* main is a function in c */
 /* return zero if successful */
 
-int main(void) {
+int main(void)
+{
    /* my first program in C */
    puts("\"Programming is like building a multilingual puzzle");
-   
-   return 0;
+   return (0);
 }
