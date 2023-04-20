fe9337900f5f57c399079a81412d3ec057b18ae0
grace842 <gwanja30@gmail.com>
2023-04-20 07:07:21 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* main is a function in c */
+/* return zero if successful */
+
+int main(void) {
+   /* my first program in C */
+   puts("\"Programming is like building a multilingual puzzle");
+   
+   return 0;
+}
